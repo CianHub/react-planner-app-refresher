9dedd96132776c373fed6ffc114a6988a16aa2f9
Cian O'Gralaigh <ihatecian@gmail.com>
2020-10-07 22:09:27 +0100

    add x buttons

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -2,26 +2,43 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 export class Task extends Component {
-    getStyle = (completed) => ({
-        textDecoration: this.props.task.completed ? 'line-through' : 'none',
-        padding: '10px',
-        borderBottom: '1px #ccc dotted',
-    });
+  getStyle = (completed) => ({
+    textDecoration: this.props.task.completed ? 'line-through' : 'none',
+    padding: '10px',
+    borderBottom: '1px #ccc dotted',
+  });
 
-    markComplete = (e) => console.log(e.target.value)
+  markComplete = (e) => console.log(e.target.value);
 
-    render() {
-        const { id, title } = this.props.task
-        return (
-            <div style={this.getStyle(this.props.completed)}>
-                <p>
-                    <input type="checkbox" onChange={this.props.markComplete.bind(this, id)} />{' '}
-                    {title}</p>
-            </div>
-        );
-    }
+  render() {
+    const { id, title } = this.props.task;
+    return (
+      <div style={this.getStyle(this.props.completed)}>
+        <p>
+          <input
+            type="checkbox"
+            onChange={this.props.markComplete.bind(this, id)}
+          />{' '}
+          {title}
+          <button style={buttonStyle}>X</button>
+        </p>
+      </div>
+    );
+  }
 }
 
-Task.propTypes = { task: PropTypes.object.isRequired, markComplete: PropTypes.func.isRequired };
+Task.propTypes = {
+  task: PropTypes.object.isRequired,
+  markComplete: PropTypes.func.isRequired,
+};
 
+const buttonStyle = {
+  background: 'red',
+  color: 'white',
+  border: 'none',
+  padding: '5px 9px',
+  borderRadius: '50%',
+  cursor: 'pointer',
+  float: 'right',
+};
 export default Task;
