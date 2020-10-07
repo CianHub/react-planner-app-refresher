af86b209a4ffd79889579a248bd5c742527edee9
Cian O'Gralaigh <ihatecian@gmail.com>
2020-10-07 21:56:21 +0100

    Add function as prop

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Planner from './components/Planner';
 class App extends React.Component {
   state = {
     tasks: [
-      { id: 1, title: 'Feed Franklin', completed: false },
+      { id: 1, title: 'Feed Franklin', completed: true },
       { id: 2, title: 'Laundry', completed: false },
       { id: 3, title: 'Clean kitchen', completed: false },
     ],
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,16 +1,26 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export class Task extends Component {
+    getStyle = (completed) => ({
+        textDecoration: this.props.task.completed ? 'line-through' : 'none',
+        padding: '10px',
+        borderBottom: '1px #ccc dotted',
+    });
+
+    markComplete = (e) => console.log(e.target.value)
+
     render() {
         return (
-            <h3>
-                {this.props.task.title}
-            </h3>
-        )
+            <div style={this.getStyle(this.props.completed)}>
+                <p>
+                    <input type="checkbox" onChange={this.markComplete} />{' '}
+                    {this.props.task.title}</p>
+            </div>
+        );
     }
 }
 
-Task.propTypes = { task: PropTypes.object.isRequired }
+Task.propTypes = { task: PropTypes.object.isRequired };
 
-export default Task
+export default Task;
