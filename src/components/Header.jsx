0158d753272e631c3a32650a5fc67e3802162457
Cian O'Gralaigh <ihatecian@gmail.com>
2020-10-08 00:10:03 +0100

    Add navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
     <header style={headerStyle}>
       <h1>Task List</h1>
+      <Link style={linkStyle} to="/">
+        Home
+      </Link>{' '}
+      |{' '}
+      <Link style={linkStyle} to="/about">
+        About
+      </Link>
     </header>
   );
 }
@@ -14,3 +22,5 @@ const headerStyle = {
   textAlign: 'center',
   padding: '10px',
 };
+
+const linkStyle = { textDecoration: 'none', color: '#fff' };
