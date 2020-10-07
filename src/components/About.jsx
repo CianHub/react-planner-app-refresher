e29c7a5ce6fe31ded21a0b6dec0d0d1c22e06b5c
Cian O'Gralaigh <ihatecian@gmail.com>
2020-10-08 00:02:15 +0100

    Add About component

---
+++ src/components/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const About = () => {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>This is a task list</p>
+    </React.Fragment>
+  );
+};
